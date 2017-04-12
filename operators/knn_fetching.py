ef7b7423c504b32f19e4fb876da97a32d6e9a992
han <hankruiger@gmail.com>
2017-04-12 15:55:04 +0200

    Made knn search faster by a factor of about 7.

--- operators/knn_fetching.py
+++ operators/knn_fetching.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 from sklearn.neighbors import NearestNeighbors
+from scipy.spatial.distance import cdist
 
 from model import Dataset
 
@@ -14,36 +15,18 @@ def knn_fetching(source, query, n_samples, sort=True, verbose=True):
     # Query data
     Y = X[query_idcs, :]
 
-    # Indices the source data on indices where the query is NOT.
+    # Retrieve the source data on indices where the query is NOT.
     # (I.e., all candidates)
     source_idcs = np.delete(np.arange(source.n_points()), query_idcs)
+    X_search = X[source_idcs, :]
 
-    # Holds indexes in X, and the distance from those observations to any query point.
-    closest = dict()
+    # Compute smallest distances from all source points to any query point.
+    dists = cdist(Y, X_search, metric='euclidean').min(axis=0)
+    # Retrieve indices (in X_search!) where the distances are smallest
+    smallest_dist_idcs = np.argpartition(dists, n_samples)[:n_samples]
+    # Get the corresponding indices in the root dataset
+    idcs_in_root = source_idcs[smallest_dist_idcs]
 
-    # Holds the distance to the furthest closest observation.
-    max_dist = -np.inf
-
-    for i in source_idcs:
-        # Compute the smallest distance from the candidate to any query sample.
-        dist = np.min(np.linalg.norm(Y - X[i, :], axis=1))
-
-        if len(closest) < n_samples:
-            # Still room for more candidates, add in any case.
-            closest[i] = dist
-            max_dist = max(max_dist, dist)
-        elif dist < max_dist:
-            # Throw the furthest candidate out
-            j = max(closest.keys(), key=lambda idx: closest[idx])
-            del closest[j]
-
-            # Add this candidate, because it's at least closer than j.
-            closest[i] = dist
-
-            # Recompute it.
-            max_dist = max(closest.values())
-
-    idcs_in_root = np.array(list(closest.keys()))
     if sort:
         idcs_in_root.sort()
     data = X[idcs_in_root, :]
