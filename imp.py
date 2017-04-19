2511457b1a2755b7f6262fbb2cea06470e14ed0f
han <hankruiger@gmail.com>
2017-04-19 14:55:15 +0200

    Earlier check on venv.

--- imp.py
+++ imp.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
+# Check if we're in a virtual environment.
+if sys.prefix == sys.base_prefix:
+    from warnings import warn
+    warn('Not using a virtual environment! Consider making/activating one. (See README.md)')
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QSurfaceFormat
 
 from widgets import *
 
-if __name__ == '__main__':
-    # Check if we're in a virtual environment.
-    if sys.prefix == sys.base_prefix:
-        from warnings import warn
-        warn('Not using a virtual environment! Consider making/activating one. (See README.md)')
-    
+if __name__ == '__main__':    
     qsf = QSurfaceFormat()
     qsf.setRenderableType(QSurfaceFormat.OpenGL)
     qsf.setProfile(QSurfaceFormat.CoreProfile)
